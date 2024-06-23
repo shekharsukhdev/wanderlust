edb4c4668f5cff8fa96ec0575cc8801dde38aa11
shekharsukhdev <shekharsukhdev@eng.rizvi.edu.in>
2024-06-24 02:49:32 +0530

    add new files

---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  description: String,
+  image: {
+    filename:String,
+    url:{
+      type:String,
+      default:
+      "https://images.unsplash.com/photo-1718226979808-874230b0c52f?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1718226979808-874230b0c52f?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+    }
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
